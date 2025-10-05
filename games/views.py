40c690f22caebbc362eb13310be21c4d9e1cbfa6
sejalparudia79@gmail.com <sejalparudia05@gmail.com>
2025-10-06 00:25:13 +0530

    Changed the layout

--- games/views.py
+++ games/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .logic.tictactoe import check_winner 
 
-# Create your views here.
+def home(request):
+    return render(request, 'games/index.html')
+
+def tic_tac_toe(request):
+    board = [[" "]*3 for _ in range(3)]
+    winner = None
+
+    return render(request, 'games/tictactoe.html', {'board': board, 'winner': winner})
+
+
