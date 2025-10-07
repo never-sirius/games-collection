c429d73ea7f4362a8b544b5ff0943f63b6d50f4d
sejalparudia79@gmail.com <sejalparudia05@gmail.com>
2025-10-07 22:51:37 +0530

    adding the tictactoe game

---
+++ games/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # You can create a placeholder views file if needed
+
+urlpatterns = [
+    # Example placeholder URL
+    # path('tictactoe/', views.tictactoe_view, name='tictactoe'),
+]
